9cbd2a4407e2c21fa459da634160e1fad7700e29
Kaito Mitomi <kaitomiy@gmail.com>
2020-09-20 22:00:25 +0900

    add stripe to backend

--- index.js
+++ index.js
@@ -13,17 +13,18 @@ mongoose.connect(keys.mongoURI, {
 
 const app = express();
 
+app.use(express.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey],
   })
 );
-
 app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('The server is listening...'));
