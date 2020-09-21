140ce7d9ffccbbd81841db37bb9c326cfaf4fcc6
Kaito Mitomi <kaitomiy@gmail.com>
2020-09-21 22:03:20 +0900

    add reduxform

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import SurveyForm from './SurveyForm';
+
 class SurveyNew extends Component {
   render() {
     return (
       <div>
-        <h1>SurveyNew</h1>
+        <SurveyForm />
       </div>
     );
   }
