26f319fa37970e646df2286213eb480a3f23870b
Kaito Mitomi <kaitomiy@gmail.com>
2020-09-21 09:41:37 +0900

    add middleware to backend & complete stripe

--- models/User.js
+++ models/User.js
@@ -3,6 +3,10 @@ const { Schema } = mongoose;
 
 const userShcema = new Schema({
   googleId: String,
+  credits: {
+    type: Number,
+    default: 0,
+  },
 });
 
 mongoose.model('users', userShcema);
