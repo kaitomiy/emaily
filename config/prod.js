0af8a9cd52c729a6ec859a4ab9365f7d89148bb4
Kaito Mitomi <kaitomiy@gmail.com>
2020-09-21 21:04:19 +0900

    add user feedback

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,4 @@
 // prod
-
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -8,4 +7,5 @@ module.exports = {
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
   sendGridKey: process.env.SEND_GRID_KEY,
+  redirectDomain: process.env.REDIRECT_DOMAIN,
 };
