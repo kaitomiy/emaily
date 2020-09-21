6437e9433e732f4978ad1cbde4c265f1ccd13c7c
Kaito Mitomi <kaitomiy@gmail.com>
2020-09-21 21:45:58 +0900

    add surveynew

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SurveyNew extends Component {
+  render() {
+    return (
+      <div>
+        <h1>SurveyNew</h1>
+      </div>
+    );
+  }
+}
+export default SurveyNew;
