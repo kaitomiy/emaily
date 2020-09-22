1ad0bd72e307b1e6061f205f6c1111a9655c3c56
Kaito Mitomi <kaitomiy@gmail.com>
2020-09-22 11:44:07 +0900

    add refactor fields

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const SurveyFormReview = () => {
+const SurveyFormReview = ({ onCancel, formValues }) => {
   return (
     <div>
       <h5>Please confirm your entries</h5>
+      <button className='yellow darken-3 btn-flat' onClick={onCancel}>
+        Back
+      </button>
     </div>
   );
 };
 
-export default SurveyFormReview;
+const mapStateToProps = (state) => {
+  return {
+    formValues: state.form.surveyForm.values,
+  };
+};
+
+export default connect(mapStateToProps)(SurveyFormReview);
