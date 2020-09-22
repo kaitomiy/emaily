b07c4944e62065b9b740243038c325732c26954c
Kaito Mitomi <kaitomiy@gmail.com>
2020-09-22 12:26:18 +0900

    add finalize review fields

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+import formFields from './formFields';
+import * as actions from '../../actions';
+
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
+  const reviewFiles = _.map(formFields, ({ name, label }) => {
+    return (
+      <div key={name}>
+        <label>{label}</label>
+        <div>{formValues[name]}</div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h5>Please confirm your entries</h5>
-      <button className='yellow darken-3 btn-flat' onClick={onCancel}>
+      {reviewFiles}
+      <button
+        className='yellow darken-3 white-text btn-flat'
+        onClick={onCancel}
+      >
         Back
+      </button>
+      <button
+        onClick={() => submitSurvey(formValues)}
+        className='green btn-flat right white-text'
+      >
+        Send Survey
+        <i className='material-icons right'>email</i>
       </button>
     </div>
   );
@@ -18,4 +42,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(SurveyFormReview);
